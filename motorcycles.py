9d1307ac4b3a067bf4d1a131cf850ec54d93e59c
macbook <macbook@blackbullet.local>
2019-07-20 22:26:33 +0700

    hapus list dan print

--- motorcycles.py
+++ motorcycles.py
@@ -38,6 +38,7 @@ print(popped_motorcycle)
 # hapus list berdasarkan value
 motorcycles = ["honda", "yamaha", "suzuki", "ducati"]
 print(motorcycles)
-motorcycles.remove("ducati")
+too_expensive = "ducati"
+motorcycles.remove(too_expensive)
 print(motorcycles)
-
+print(f"\nA {too_expensive.title()} is too expensive for me.")
