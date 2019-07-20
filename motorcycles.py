719762c3dfd79dd8f4086a09058c3999a02df4bc
macbook <macbook@blackbullet.local>
2019-07-20 21:08:32 +0700

    appending list

--- motorcycles.py
+++ motorcycles.py
@@ -2,6 +2,11 @@ motorcycles = ["honda", "yamaha", "suzuki"]
 print(motorcycles)
 
 # mengubah isi list ke 1
-motorcycles[0] = "ducati"
+# motorcycles[0] = "ducati"
+# print(motorcycles)
+
+
+# appending list
+motorcycles.append("ducati")
 print(motorcycles)
 
