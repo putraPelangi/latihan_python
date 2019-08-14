65739ea6abce4bc2efc61eb510adc3eeb477a014
macbook <macbook@blackbullet.local>
2019-08-14 13:41:14 +0700

    sorting

---
+++ cars.py
@@ -0,0 +1,23 @@
+cars = ["bmw", "audi", "toyota", "subaru"]
+
+# permanen Sort
+# cars.sort()
+# print(cars)
+
+# Permanaen Reverse sort
+# cars.sort(reverse=True)
+# print(cars)
+
+
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
+
+# reverse print
+print(cars)
+cars.reverse()
+print(cars)
+
