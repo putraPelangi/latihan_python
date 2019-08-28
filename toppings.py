58753e3a0f11bdd64001b9fb3930a016ef34c26f
macbook <macbook@blackbullet.local>
2019-08-28 09:36:51 +0700

    if else list

--- toppings.py
+++ toppings.py
@@ -1,7 +1,10 @@
 requested_topping = "mushrooms"
 if requested_topping != "anchovies":
     print("Hold the anchovies!")
-requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+requested_toppings = ["mushrooms", "green peppers", "extra cheese"]
 for requested_topping in requested_toppings:
-print(f"Adding {requested_topping}.")
+    if requested_topping == "green peppers":
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
 print("\nFinished making your pizza!")
