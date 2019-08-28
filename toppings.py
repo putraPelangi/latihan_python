9793c910870ef87ba617fd80a45a75d322614a9b
macbook <macbook@blackbullet.local>
2019-08-28 09:45:51 +0700

    if else list topping

--- toppings.py
+++ toppings.py
@@ -8,3 +8,13 @@ for requested_topping in requested_toppings:
     else:
         print(f"Adding {requested_topping}.")
 print("\nFinished making your pizza!")
+
+
+# Checking That a List Is Not Empty
+requested_toppings = []
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza!")
+else:
+    print("\nAre you sure you want a plain pizza?")
