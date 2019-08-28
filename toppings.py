2ba1bc42ff2d13f1c6806aeea97f729d009f5f15
macbook <macbook@blackbullet.local>
2019-08-28 09:31:41 +0700

    if with list

--- toppings.py
+++ toppings.py
@@ -1,3 +1,7 @@
 requested_topping = "mushrooms"
 if requested_topping != "anchovies":
     print("Hold the anchovies!")
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
