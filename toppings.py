35615f270103e00734558b7dedbcda704dbb48ea
macbook <macbook@blackbullet.local>
2019-08-28 09:53:55 +0700

    sing Multiple Lists

--- toppings.py
+++ toppings.py
@@ -17,4 +17,23 @@ if requested_toppings:
         print(f"Adding {requested_topping}.")
     print("\nFinished making your pizza!")
 else:
-    print("\nAre you sure you want a plain pizza?")
+    print("\nAre you sure you want a plain pizza?\n")
+
+
+# Using Multiple Lists
+
+available_toppings = [
+    "mushrooms",
+    "olives",
+    "green peppers",
+    "pepperoni",
+    "pineapple",
+    "extra cheese",
+]
+requested_toppings = ["mushrooms", "french fries", "extra cheese"]
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+print("\nFinished making your pizza!")
