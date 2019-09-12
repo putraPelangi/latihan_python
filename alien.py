07b6f16820354e47fc47d26f43c67e3c35521a9e
macbook <macbook@blackbullet.local>
2019-09-12 13:22:10 +0700

    looping directory

--- alien.py
+++ alien.py
@@ -1,11 +1,82 @@
-warna_aliens = ["green", "red", "yellow"]
+alien_0 = {"color": "green", "points": 5}
+new_points = alien_0["points"]
+print(alien_0["color"])
+print(f"You just earned {new_points} points!\n")
 
-if "green" in warna_aliens:
-    poin = 10
-elif "yellow" in warna_aliens:
-    poin = 15
-elif "red" in warna_aliens:
-    poin = 20
+# Adding New Key-Value Pairs
+alien_0 = {"color": "green", "points": 5}
+print(alien_0)
+alien_0["x_position"] = 0
+alien_0["y_position"] = 25
+print(alien_0)
+
+# Starting with an Empty Dictionary
+print("\n")
+alien_0 = {}
+alien_0["color"] = "green"
+alien_0["points"] = 5
+print(alien_0)
+
+
+# Modifying Values in a Dictionary
+print("\n")
+alien_0 = {"color": "green"}
+print(f"The alien is {alien_0['color']}.")
+alien_0["color"] = "yellow"
+print(f"The alien is now {alien_0['color']}.")
+
+# Modifying 2
+print("\n")
+alien_0 = {"x_position": 0, "y_position": 25, "speed": "medium"}
+print(f"Original position: {alien_0['x_position']}")
+# Move the alien to the right.
+# Determine how far to move the alien based on its current speed.
+if alien_0["speed"] == "slow":
+    x_increment = 1
+elif alien_0["speed"] == "medium":
+    x_increment = 2
 else:
-    poin = 0
-print(f"you earns ${poin} point in this game")
+    # This must be a fast alien.
+    x_increment = 3
+# The new position is the old position plus the increment.
+alien_0["x_position"] = alien_0["x_position"] + x_increment
+print(f"New position: {alien_0['x_position']}")
+
+# Removing Key-Value Pairs
+alien_0 = {"color": "green", "points": 5}
+print(alien_0)
+del alien_0["points"]
+print(alien_0)
+
+# A List of Dictionaries
+alien_0 = {"color": "green", "points": 5}
+alien_1 = {"color": "yellow", "points": 10}
+alien_2 = {"color": "red", "points": 15}
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
+# Make an empty list for storing aliens.
+print("\n")
+aliens = []
+# Make 30 green aliens.
+for alien_number in range(30):
+    new_alien = {"color": "green", "points": 5, "speed": "slow"}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien["color"] == "green":
+        alien["color"] = "yellow"
+        alien["speed"] = "medium"
+        alien["points"] = 10
+    elif alien["color"] == "yellow":
+        alien["color"] = "red"
+        alien["speed"] = "fast"
+        alien["points"] = 15
+# Show the first 5 aliens.
+for alien in aliens[:5]:
+    print(alien)
+print("...")
+# Show how many aliens have been created.
+print(f"Total number of aliens: {len(aliens)}")
+
---
+++ favorite_languages.py
@@ -0,0 +1,45 @@
+favorite_languages = {"jen": "python", "sarah": "c", "edward": "ruby", "phil": "python"}
+language = favorite_languages["sarah"].title()
+print(f"Sarah's favorite language is {language}.")
+
+
+# Looping Through All Key-Value Pairs
+favorite_languages = {"jen": "python", "sarah": "c", "edward": "ruby", "phil": "python"}
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# Looping Through All the Keys in a Dictionary
+favorite_languages = {"jen": "python", "sarah": "c", "edward": "ruby", "phil": "python"}
+for name in favorite_languages.keys():  # same for name in favorite_languages
+    print(name.title())
+
+# linked with language favorite
+friends = ["phil", "sarah"]
+print("\n")
+for name in favorite_languages.keys():
+    print(name.title())
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+# returns a list of all the keys
+favorite_languages = {"jen": "python", "sarah": "c", "edward": "ruby", "phil": "python"}
+if "erin" not in favorite_languages.keys():
+    print("\nErin, please take our poll!")
+
+# sorting Looping Through a Dictionary’s Keys in a Particular Order
+
+favorite_languages = {"jen": "python", "sarah": "c", "edward": "ruby", "phil": "python"}
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking the poll.\n")
+
+# Looping Through All Values in a Dictionary
+favorite_languages = {"jen": "python", "sarah": "c", "edward": "ruby", "phil": "python"}
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+# cek duplikat dengan set
+print("\n   The following languages have been mentioned:")
+for language in set(favorite_languages.values()):
+    print(language.title())
