da5afdfe56d296b3e60373d42f2bfc03d45bbb4e
macbook <macbook@blackbullet.local>
2020-03-02 08:13:01 +0700

    python add list on directory

--- favorite_languages.py
+++ favorite_languages.py
@@ -43,3 +43,17 @@ for language in favorite_languages.values():
 print("\n   The following languages have been mentioned:")
 for language in set(favorite_languages.values()):
     print(language.title())
+
+
+print("\n\n")
+# list on dictionary
+favorite_languages = {
+    "jen": ["python", "ruby"],
+    "sarah": ["c"],
+    "edward": ["ruby", "go"],
+    "phil": ["python", "haskell"],
+}
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+    for language in languages:
+        print(f"\t{language.title()}")
